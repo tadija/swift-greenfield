b1c765cfe632804674c9a0a58c7a6fd599829b60
Marko Tadic <tadija@me.com>
2022-12-08 18:47:55 +0100

    Initial Commit

---
+++ Greenfield/GreenfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenfieldApp.swift
+//  Greenfield
+//
+//  Created by Marko Tadić on 8.12.22..
+//
+
+import SwiftUI
+
+@main
+struct GreenfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
